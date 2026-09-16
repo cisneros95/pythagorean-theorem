5e8887400c3d134087d96d03a90fa214c903ab94
cisneros95 <raul.f.cisneros@outlook.com>
2026-09-16 15:01:13 -0700

    Update Pythagorean Theorem project: 2026-09-16 15:01

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pythagorean Theorem Calculator</title>
+    <title>Pythagorean Theorem</title>
 </head>
 <body>
     <h1>Pythagorean Theorem Calculator</h1>
